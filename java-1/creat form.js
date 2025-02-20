fa740c1901b0741a2e7f5dd97433d742ae673383
Sathe-Khanom <sathekhanom550@gmail.com>
2025-02-20 13:43:32 +0600

    add

---
+++ java-1/creat form.js
@@ -0,0 +1,25 @@
+
+
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
+
+
+let gender = document.querySelector('input[name:"gender"]:checked');
+let hobby = document.querySelectorAll('input[name:"hobby"]:checked');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
